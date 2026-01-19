2b6edd348468428c1575538862050ce2dacea9e5
Subhajit Sahu <wolfram77@gmail.com>
2026-01-19 11:42:58 +0530

    :bug: update version header

---
+++ CXXGraph/version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/// Populate version macros (@wolfram77)
+#define CXXGraph_VERSION "4.1.0"
+#define CXXGraph_VERSION_MAJOR "4"
+#define CXXGraph_VERSION_MINOR "1"
+#define CXXGraph_VERSION_PATCH "0"
